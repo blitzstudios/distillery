3d790410670010ddc27ab713e7dd4ae018d37f7e
Eric Brown <erickeithbrown@gmail.com>
2023-07-19 21:30:42 -0700

    disable code path pruning

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule Distillery.Mixfile do
       dialyzer_ignored_warnings: [
         {:warn_contract_supertype, :_, {:contract_supertype, [:_, :impl_for, 1, :_, :_]}},
         {:warn_contract_supertype, :_, {:contract_supertype, [:_, :impl_for!, 1, :_, :_]}}
-      ]
+      ],
+      prune_code_paths: false
     ]
   end
 
